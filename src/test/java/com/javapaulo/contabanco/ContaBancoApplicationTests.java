6bf1ad1e6ed313f7784c101fd7e0ddd469eebc0d
paulorsp2021 <paulorsp2021@gmail.com>
2025-03-27 00:55:33 -0300

    inicial

---
+++ src/test/java/com/javapaulo/contabanco/ContaBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapaulo.contabanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
